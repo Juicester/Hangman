18d190b8706837858d79398d551e95e6cdc4c1cd
justinpedowitz <justin@pedowitz.net>
2020-04-10 13:49:36 -0700

    Got the game logic working other than the victory

--- hangmanscript.js
+++ hangmanscript.js
@@ -1,1 +1,38 @@
-console.log('hello');
+//console.log('hello');
+
+let word = 'sbitches';
+
+const wordArea = document.querySelector('.word-area');
+
+let splitWord = word.split('');
+console.log(splitWord);
+wordArea.style.gridTemplateColumns = `repeat(${splitWord.length}, 8vmin)`;
+
+for (let i = 0; i < splitWord.length; i++) {
+	let div = document.createElement('div');
+	div.classList.add('letter-square');
+	div.setAttribute('data-id', i);
+	div.innerText = splitWord[i];
+	wordArea.appendChild(div);
+}
+
+const guessButton = document.querySelector('.letter-guess-button');
+// const guessArea = document.querySelector('.letter-guess');
+
+guessButton.addEventListener('click', letterGuess);
+
+function letterGuess(event) {
+	const userGuess = document.querySelector('.user-letter-guess').value;
+	console.log(userGuess);
+
+	const letterBoxes = document.querySelectorAll('.letter-square');
+	for (let i = 0; i < splitWord.length; i++) {
+		if (userGuess.toUpperCase() == splitWord[i].toUpperCase()) {
+			letterBoxes[i].style.color = 'black';
+			// console.log('its the same');
+		} else {
+			// console.log('not the same');
+		}
+	}
+	//userGuess = '';
+}
