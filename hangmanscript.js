fe02b764afe88c104d50af9452ec105f2790d753
justinpedowitz <justin@pedowitz.net>
2020-04-14 15:31:12 -0700

    added more style

--- hangmanscript.js
+++ hangmanscript.js
@@ -4,35 +4,35 @@ word = word.toLowerCase();
 const wordArea = document.querySelector('.word-area');
 const wordSubmitButton = document.querySelector('.word-submit-button');
 
-wordSubmitButton.addEventListener('click', wordSubmit);
+// wordSubmitButton.addEventListener('click', wordSubmit);
 
-function wordSubmit() {
-	let wordInput = document.querySelector('.user-word-input').value;
-	console.log(wordInput);
+// function wordSubmit() {
+// 	let wordInput = document.querySelector('.user-word-input').value;
+// 	console.log(wordInput);
 
-	let splitWordInput = wordInput.split('');
+// 	let splitWordInput = wordInput.split('');
 
-	wordArea.style.gridTemplateColumns = `repeat(${splitWord.length}, 8vmin)`;
-	for (let i = 0; i < splitWordInput.length; i++) {
-		let div = document.createElement('div');
-		div.classList.add('letter-square');
-		// div.setAttribute('data-id', i);
-		div.innerText = splitWordInput[i];
-		wordArea.appendChild(div);
-	}
-}
+// 	wordArea.style.gridTemplateColumns = `repeat(${splitWord.length}, 8vmin)`;
+// 	for (let i = 0; i < splitWordInput.length; i++) {
+// 		let div = document.createElement('div');
+// 		div.classList.add('letter-square');
+// 		// div.setAttribute('data-id', i);
+// 		div.innerText = splitWordInput[i];
+// 		wordArea.appendChild(div);
+// 	}
+// }
 
 let splitWord = word.split('');
 console.log(splitWord);
-// wordArea.style.gridTemplateColumns = `repeat(${splitWord.length}, 8vmin)`;
+wordArea.style.gridTemplateColumns = `repeat(${splitWord.length}, 8vmin)`;
 
-// for (let i = 0; i < splitWord.length; i++) {
-// 	let div = document.createElement('div');
-// 	div.classList.add('letter-square');
-// 	// div.setAttribute('data-id', i);
-// 	div.innerText = splitWord[i];
-// 	wordArea.appendChild(div);
-// }
+for (let i = 0; i < splitWord.length; i++) {
+	let div = document.createElement('div');
+	div.classList.add('letter-square');
+	// div.setAttribute('data-id', i);
+	div.innerText = splitWord[i];
+	wordArea.appendChild(div);
+}
 
 const guessButton = document.querySelector('.letter-guess-button');
 // const guessArea = document.querySelector('.letter-guess');
